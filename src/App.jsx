86eeceda750c33389eb982c369ed934e63105ba8
EiG123 <narinsuwan567@gmail.com>
2024-02-18 14:27:41 +0700

    data & props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Item from "./components/Item";
+import logo from "./assets/logo.png"
+import content from "./data/content";
 export default function App() {
   return (
     <div className="App">
+      <img src={logo} alt="logo" />
       <h2>React + Vite</h2>
       <p>EiG123</p>
-      <Item/>
-      <Item/>
+      {content.map((data)=>{
+        return(
+          <Item key={data.id} {...data}/>
+        );
+      })}
     </div>
   );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,13 @@
-export default function Item(){
-    return(
-        <section>
-            <div className="content">
-                <h4>รายละเอียดบทความ</h4>
-            </div>
-        </section>
-    );
+export default function Item(props) {
+  const {id, title, description} = props
+  return (
+    <section>
+      <div className="content">
+        <h4>{title}</h4>
+        <button>Show</button>
+      </div>
+      <p>{description}</p>
+      <hr/>
+    </section>
+  );
 }
